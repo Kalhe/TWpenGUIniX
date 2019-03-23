38ce4cbf1e5b8aa6de9858a717a5c65a12c49dbe
Kalhe <48484509+Kalhe@users.noreply.github.com>
2019-03-23 02:40:23 +0200

    Add files via upload

---
+++ Site Propriu-zis/ButtonScripts.js
@@ -0,0 +1,11 @@
+function button1(){
+	window.alert("Ai apasat primul buton!");
+}
+
+function button2(){
+	window.alert("Ai apasat al doilea buton!");
+}
+
+function button3(){
+	window.alert("Ai apasat al treilea buton!");
+}
